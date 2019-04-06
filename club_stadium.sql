34f3504b429cb6a4204e3578cf37a66a2957611e
Sahil Dhull <sahildhull25121998@gmail.com>
2019-04-07 00:00:34 +0530

    Insertions added

---
+++ club_stadium.sql
@@ -0,0 +1,40 @@
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(3,1);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(13,2);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(10,3);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(18,4);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(2,5);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(7,6);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(8,7);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(5,8);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(16,9);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(9,10);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(11,11);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(19,12);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(4,13);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(12,14);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(6,15);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(14,16);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(1,17);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(15,18);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(17,19);
+insert into CLUB_STADIUM(CLUB_ID,STADIUM_ID)
+	values(20,20);
---
+++ stadiums.sql
@@ -0,0 +1,40 @@
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Amex Stadium",22374,"Latitude=50.860901,Longitude=-0.08014");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("St James Park",52387,"Latitude=54.975601,Longitude=-1.62179");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Anfield",45522,"Latitude=53.430801,Longitude=-2.96096");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Vicarage Road",17477,"Latitude=51.649799,Longitude=-0.401569");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Seward Stadium",10700,"Latitude=50.735199,Longitude=-1.83839");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Goodison Park",40157,"Latitude=53.438702,Longitude=-2.96619");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("The Galpharm Stadium",24500,"Latitude=53.654301,Longitude=-1.76837");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Stamford Bridge",41837,"Latitude=51.481602,Longitude=-0.191034");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Liberty Stadium",20532,"Latitude=51.642799,Longitude=-3.93473");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("King Power Stadium",32262,"Latitude=52.6203,Longitude=-1.14217");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Etihad Stadium",47805,"Latitude=53.483002,Longitude=-2.20024");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("The Hawthorns",26272,"Latitude=52.508999,Longitude=-1.96418");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Turf Moor",22546,"Latitude=53.788799,Longitude=-2.23018");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Old Trafford",75811,"Latitude=53.4631,Longitude=-2.29139");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Selhurst Park",26309,"Latitude=51.3983,Longitude=-0.085455");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("St Mary's Stadium",32689,"Latitude=50.9058,Longitude=-1.39114");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Emirates Stadium",60361,"Latitude=51.554901,Longitude=-0.108436");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Britannia Stadium",27902,"Latitude=52.9884,Longitude=-2.17542");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("White Hart Lane",36230,"Latitude=51.603298,Longitude=-0.065684");
+insert into STADIUM(STADIUM_NAME,CAPACITY,LOCATION)
+	values("Olympic Stadium",60000,"Latitude=51.5383,Longitude=-0.016587");
