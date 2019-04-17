66d230fde23fa864765f158c5762fe346684989e
Sahil Dhull <sahildhull25121998@gmail.com>
2019-04-18 01:44:36 +0530

    data inserted

--- booking.sql
+++ booking.sql
@@ -1,4 +1,8 @@
-insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE)values(2,46,'Y');
-insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE)values(2,46,'Y');
-insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE)values(4,47,'R');
-insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE)values(4,7,'Y');
+insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE,BOOKING_TIME)
+	values(2,46,'Y',40);
+insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE,BOOKING_TIME)
+	values(2,46,'Y',60);
+insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE,BOOKING_TIME)
+	values(4,47,'R',5);
+insert into booking(MATCH_ID,PLAYER_ID,BOOKING_TYPE,BOOKING_TIME)
+	values(4,7,'Y',67);
