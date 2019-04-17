fffeebfaeea9d81e77e40982d0fe42dcb9618cc9
Sahil Dhull <sahildhull25121998@gmail.com>
2019-04-18 02:09:22 +0530

    finish status added

---
+++ finish.sql
@@ -0,0 +1,2 @@
+update match set STATUS = 'Finished' where STATUS = 'Ongoing'
+	AND MATCH_ID!=6;
--- season.sql
+++ season.sql
@@ -1,4 +1,5 @@
 insert into SEASON(SEASON_ID,STATUS)
 	values(1,'Ongoing');
-insert into SEASON(SEASON_ID,STATUS)
-	values(2,'Ongoing');
+
+-- insert into SEASON(SEASON_ID,STATUS)
+-- 	values(2,'Ongoing');
